ea88a39cbfe0921d442c84021c1c5279d24f188f
Nick <pretki123@ukr.net>
2023-07-09 19:08:03 -0600

    created func and model of movies for fetching api

---
+++ NetflixCloneAppUIKit/Managers/APICaller.swift
@@ -0,0 +1,34 @@
+//
+//  APICaller.swift
+//  NetflixCloneAppUIKit
+//
+//  Created by Nick Pavlov on 7/9/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_Key = "8fc626b9b34342fd29749f14d1e6db2e"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_Key)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+    }
+}
