0e9419f55f762bc4b57953980eec136955da58bb
Nick <pretki123@ukr.net>
2023-07-10 20:40:48 -0600

    fetched data for search section

---
+++ NetflixCloneAppUIKit/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultsViewController.swift
+//  NetflixCloneAppUIKit
+//
+//  Created by Nick Pavlov on 7/10/23.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+}
