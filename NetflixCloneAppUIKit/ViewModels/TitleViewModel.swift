7aab06ac2d83f0d6c100a080a25a0a03effaba98
Nick <pretki123@ukr.net>
2023-07-10 18:39:21 -0600

    done with coming soon Section

---
+++ NetflixCloneAppUIKit/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixCloneAppUIKit
+//
+//  Created by Nick Pavlov on 7/10/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
