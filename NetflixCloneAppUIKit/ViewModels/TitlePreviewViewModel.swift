6ba2c6e22cde9c9e19409151be2ab56c4a319bc5
Nick <pretki123@ukr.net>
2023-07-11 15:08:29 -0600

    done with viewVideo from youtube

---
+++ NetflixCloneAppUIKit/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  NetflixCloneAppUIKit
+//
+//  Created by Nick Pavlov on 7/11/23.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeVideo: VideoElement
+    let titleOverview: String
+}
--- NetflixCloneAppUIKit/Views/CollectionViewTableViewCell.swift
+++ NetflixCloneAppUIKit/Views/CollectionViewTableViewCell.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
+protocol CollectionViewTableViewCellDelegate: AnyObject {
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel)
+}
+
 class CollectionViewTableViewCell: UITableViewCell {
     static let identifier = "CollectionViewTableViewCell"
+    
+    weak var delegate: CollectionViewTableViewCellDelegate?
     
     private var titles: [Title] = [Title]()
     
@@ -62,4 +68,24 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
         return titles.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.original_title ?? title.original_name else { return }
+        
+        APICaller.shared.getYoutubeMovie(with: titleName + " trailer") { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                let title = self?.titles[indexPath.row]
+                guard let titleOverview = title?.overview else { return }
+                guard let strongSelf = self else { return }
+                
+                let viewModel = TitlePreviewViewModel(title: titleName, youtubeVideo: videoElement, titleOverview: titleOverview)
+                self?.delegate?.collectionViewTableViewCellDidTapCell(strongSelf, viewModel: viewModel)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
